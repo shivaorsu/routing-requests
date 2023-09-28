5d8c757a4defe418df518026efd4c4e7c26e22b8
shivaorsu <shivaorsu785@gmail.com>
2023-09-28 15:29:25 +0530

    fourth

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const http = require("http");
+const http = require('http');
 
-const routes=require("./routes");
-console.log(routes.sometext)
+const express = require('express');
 
-const server = http.createServer(routes.handler) 
- 
- 
-  
+const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
